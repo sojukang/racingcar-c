43dcdd3b22820465880cf3b87bf69c93deb4210e
sojukang <kangsburg@gmail.com>
2023-04-09 16:05:37 +0900

    feat: add char* name to Car struct

--- modules/car.h
+++ modules/car.h
@@ -3,14 +3,23 @@
 
 struct Car{
     int position;
+    char* name;
 };
 
-struct Car* getCars(int size) {
+struct Car *getCars(int size, char **carNames) {
     struct Car* cars = malloc(size * sizeof(struct Car));
     for (int i = 0; i < size; ++i) {
         cars[i].position = 1;
+        cars[i].name = carNames[i];
     }
     return cars;
 }
 
+int sizeOf(struct Car *cars) {
+    int i = 0;
+    while (cars[i].name != NULL) {
+        i++;
+    }
+    return i;
+}
 #endif
